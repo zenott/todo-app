b0f54d1a78867bec1d36b47f04e7342eee67296e
zenott <mblevel@gmail.com>
2019-07-10 11:02:27 +0200

    Add layout

---
+++ src/UIComponents/layout.js
@@ -0,0 +1,40 @@
+import events from '../events';
+import buildProjectForm from './project';
+import buildNewTodoForm from './todo';
+
+const buildLayout = () => {
+  const title = document.createElement('h1');
+  title.classList.add('title');
+
+  const main = document.createElement('div');
+  main.classList.add('main');
+
+  const projects = document.createElement('div');
+  projects.classList.add('projects');
+
+  const projectform = buildProjectForm();
+  projects.appendChild(projectform);
+
+  const todos = document.createElement('div');
+  todos.classList.add('todos');
+
+  const todoForm = buildNewTodoForm();
+  todos.appendChild(todoForm);
+
+  main.appendChild(projects);
+  main.appendChild(todos);
+
+  const layout = document.createDocumentFragment();
+  layout.appendChild(title);
+  layout.appendChild(main);
+
+  return layout;
+};
+
+const renderLayout = () => {
+  const root = document.querySelector('#root');
+  const layout = buildLayout();
+  root.appendChild(layout);
+};
+
+events.on('init', renderLayout);
